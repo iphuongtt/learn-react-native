918f1143466767cc39897b33bd64db6487459586
Trinh Thanh Phuong <iphuongtt@gmail.com>
2019-12-18 16:44:01 +0700

    form: formik: login form

--- App/Screens/SignInScreen.js
+++ App/Screens/SignInScreen.js
@@ -1,25 +1,120 @@
-import React from 'react';
+import React, { useState, useCallBack, Fragment } from 'react';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 import {
-  View,
-  Text,
-  Button,
+    StyleSheet,
+    SafeAreaView,
+    View,
+    TouchableOpacity,
+    Platform
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Button } from 'react-native-elements';
+import FormButton from '../Components/FormButton';
+import FormInput from '../Components/FormInput';
+import ErrorMessage from '../Components/ErrorMessage';
+
+const iconPrefix = Platform.OS === 'ios' ? 'ios-' : 'md-';
 
 const SignInScreen = (props) => {
+  const [rightIcon, setRightIcon] = useState(`${iconPrefix}eye-off`);
+  const [passwordVisibility, setPasswordVisibility] = useState(false);
+
+  const { navigation } = props;
+  //const gotoSignup = useCallBack(() => {navigation.navigate('App')}, []);
+  const gotoSignup = () => {navigation.navigate('App')};
   const _signInAsync = async () => {
     await AsyncStorage.setItem('userToken', 'abc');
-    props.navigation.navigate('App');
+    console.log('test')
+    navigation.navigate('App');
   }
+
+  const handlePasswordVisibility = () => {
+    setRightIcon(v => v === `${iconPrefix}eye` ? `${iconPrefix}eye-off` : `${iconPrefix}eye`)
+    setPasswordVisibility(v => !v)
+  }
+
   return (
-    <View>
-      <Text>This is signin screen</Text>
+    <SafeAreaView>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={_signInAsync}
+        validationSchema={yup.object().shape({
+          email: yup
+            .string()
+            .email()
+            .required('Please enter a registered email'),
+          password: yup
+            .string()
+            .min(6)
+            .required()
+        })}
+      >
+        {({
+          values,
+          handleChange,
+          handleSubmit,
+          errors,
+          isValid,
+          isSubmitting,
+          handleBlur,
+          touched
+        }) => (
+          <Fragment>
+            <FormInput
+              name="email"
+              value={values.email}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              placeholder="Enter email"
+              autoCapitalize="none"
+              iconName="md-mail"
+              iconColor="#2C384A"
+            />
+            <ErrorMessage errorValue={touched.email && errors.email} />
+
+            <FormInput
+              name="password"
+              value={values.password}
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              placeholder="Enter password"
+              autoCapitalize="none"
+              iconName="md-lock"
+              iconColor="#2C384A"
+              secureTextEntry={!passwordVisibility}
+              rightIcon={
+                <TouchableOpacity onPress={handlePasswordVisibility}>
+                  <Ionicons name={rightIcon} size={28} color='grey' />
+                </TouchableOpacity>
+              }
+            />
+            <ErrorMessage errorValue={touched.password && errors.password} />
+
+            <View>
+              <FormButton
+                buttonType="outline"
+                onPress={handleSubmit}
+                title="LOGIN"
+                buttonColor="#039BE5"
+                disabled={!isValid || isSubmitting}
+                loading={isSubmitting}
+              />
+            </View>
+          </Fragment>
+        )}
+      </Formik>
       <Button
-        title="Sign in!"
-        onPress={_signInAsync}
+        title="Don't have an account? Sign Up"
+        onPress={gotoSignup}
+        titleStyle={{
+          color: "#F57C00"
+        }}
+        type="clear"
       />
-    </View>
+    </SafeAreaView>
   )
-};
+}
 
 export default SignInScreen;
